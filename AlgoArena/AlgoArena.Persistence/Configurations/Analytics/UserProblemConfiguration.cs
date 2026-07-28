670cff4959e99a869fdb5ff44cc28452662affa9
Himanshu Maurya <himanshumaurya7781@gmail.com>
2026-07-28 16:31:13 +0530

    Add configuration classes for auditable entities for analytics

---
+++ AlgoArena/AlgoArena.Persistence/Configurations/Analytics/UserProblemConfiguration.cs
@@ -0,0 +1,46 @@
+﻿using AlgoArena.Domain.Entities.Analytics;
+using AlgoArena.Persistence.Configurations.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AlgoArena.Persistence.Configurations.Analytics
+{
+    /// <summary>
+    /// Configures the UserProblem entity.
+    /// </summary>
+    public sealed class UserProblemConfiguration : AuditableEntityConfiguration<UserProblem>
+    {
+        public override void Configure(EntityTypeBuilder<UserProblem> builder)
+        {
+            base.Configure(builder);
+
+            builder.ToTable("UserProblems");
+
+            builder.Property(x => x.SubmissionCount)
+                .HasDefaultValue(0);
+
+            builder.Property(x => x.IsAttempted)
+                .HasDefaultValue(false);
+
+            builder.Property(x => x.IsSolved)
+                .HasDefaultValue(false);
+
+            builder.HasIndex(x => new
+            {
+                x.UserId,
+                x.ProblemId
+            })
+            .IsUnique();
+
+            builder.HasOne(x => x.User)
+                .WithMany(x => x.UserProblems)
+                .HasForeignKey(x => x.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(x => x.Problem)
+                .WithMany(x => x.UserProblems)
+                .HasForeignKey(x => x.ProblemId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
