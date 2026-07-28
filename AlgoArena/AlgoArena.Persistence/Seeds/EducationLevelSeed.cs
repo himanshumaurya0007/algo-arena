628143208bbe3ae43a3ef940eef6ea6f1f79c141
Himanshu Maurya <himanshumaurya7781@gmail.com>
2026-07-28 16:33:10 +0530

    Add seed classes for user entities

---
+++ AlgoArena/AlgoArena.Persistence/Seeds/EducationLevelSeed.cs
@@ -0,0 +1,46 @@
+﻿using AlgoArena.Domain.Entities.Lookups;
+
+namespace AlgoArena.Persistence.Seeds
+{
+    public static class EducationLevelSeed
+    {
+        public static IEnumerable<EducationLevel> Data =>
+        [
+            new()
+            {
+                Id = Guid.Parse("77777777-7777-7777-7777-777777777771"),
+                Name = "High School",
+                DisplayOrder = 1,
+                IsActive = true
+            },
+            new()
+            {
+                Id = Guid.Parse("77777777-7777-7777-7777-777777777772"),
+                Name = "Diploma",
+                DisplayOrder = 2,
+                IsActive = true
+            },
+            new()
+            {
+                Id = Guid.Parse("77777777-7777-7777-7777-777777777773"),
+                Name = "Bachelor's Degree",
+                DisplayOrder = 3,
+                IsActive = true
+            },
+            new()
+            {
+                Id = Guid.Parse("77777777-7777-7777-7777-777777777774"),
+                Name = "Master's Degree",
+                DisplayOrder = 4,
+                IsActive = true
+            },
+            new()
+            {
+                Id = Guid.Parse("77777777-7777-7777-7777-777777777775"),
+                Name = "Doctorate",
+                DisplayOrder = 5,
+                IsActive = true
+            }
+        ];
+    }
+}
