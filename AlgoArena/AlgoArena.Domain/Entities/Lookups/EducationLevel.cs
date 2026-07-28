c8666e349f2907d708a979b268b6ce6f03531a8b
Himanshu Maurya <himanshumaurya7781@gmail.com>
2026-07-28 16:17:41 +0530

    Add lookup entity classes for user model

---
+++ AlgoArena/AlgoArena.Domain/Entities/Lookups/EducationLevel.cs
@@ -0,0 +1,13 @@
+﻿using AlgoArena.Domain.Common;
+using AlgoArena.Domain.Entities.Profiles;
+
+namespace AlgoArena.Domain.Entities.Lookups
+{
+    /// <summary>
+    /// Represents an education level.
+    /// </summary>
+    public sealed class EducationLevel: LookupEntity
+    {
+        public ICollection<EducationDetail> EducationDetails { get; set; } = new List<Profiles.EducationDetail>();
+    }
+}
---
+++ AlgoArena/AlgoArena.Domain/Entities/Lookups/ExperienceLevel.cs
@@ -0,0 +1,13 @@
+﻿using AlgoArena.Domain.Common;
+using AlgoArena.Domain.Entities.Profiles;
+
+namespace AlgoArena.Domain.Entities.Lookups
+{
+    /// <summary>
+    /// Represents a professional experience level.
+    /// </summary>
+    public sealed class ExperienceLevel: LookupEntity
+    {
+        public ICollection<ProfessionalDetail> ProfessionalDetails { get; set; } = new List<Profiles.ProfessionalDetail>();
+    }
+}
