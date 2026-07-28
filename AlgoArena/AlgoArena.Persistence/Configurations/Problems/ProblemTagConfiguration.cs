fe0db3b881faa932a92eb17c3e9f4b6771d3313d
Himanshu Maurya <himanshumaurya7781@gmail.com>
2026-07-28 16:29:37 +0530

    Add configuration classes for auditable entities for problems

---
+++ AlgoArena/AlgoArena.Persistence/Configurations/Problems/ProblemTagConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using AlgoArena.Domain.Entities.Problems;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AlgoArena.Persistence.Configurations.Problems
+{
+    public sealed class ProblemTagConfiguration : IEntityTypeConfiguration<ProblemTag>
+    {
+        public void Configure(EntityTypeBuilder<ProblemTag> builder)
+        {
+            builder.ToTable("ProblemTags");
+
+            builder.HasKey(x => new
+            {
+                x.ProblemId,
+                x.TagId
+            });
+
+            builder.HasOne(x => x.Problem)
+                .WithMany(x => x.ProblemTags)
+                .HasForeignKey(x => x.ProblemId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(x => x.Tag)
+                .WithMany(x => x.ProblemTags)
+                .HasForeignKey(x => x.TagId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
