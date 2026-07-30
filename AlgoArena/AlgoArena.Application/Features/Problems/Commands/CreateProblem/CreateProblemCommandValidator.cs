403201989734929028337b99eb6c3dd0a3b9241d
Himanshu Maurya <himanshumaurya7781@gmail.com>
2026-07-30 17:18:29 +0530

    Add CreateProblem command, handler, and validator for problem creation

---
+++ AlgoArena/AlgoArena.Application/Features/Problems/Commands/CreateProblem/CreateProblemCommandValidator.cs
@@ -0,0 +1,40 @@
+﻿using FluentValidation;
+
+namespace AlgoArena.Application.Features.Problems.Commands.CreateProblem
+{
+    /// <summary>
+    /// Validator for CreateProblemCommand.
+    /// </summary>
+    public sealed class CreateProblemCommandValidator
+        : AbstractValidator<CreateProblemCommand>
+    {
+        public CreateProblemCommandValidator()
+        {
+            RuleFor(x => x.ProgrammingDomainId)
+                .NotEmpty();
+
+            RuleFor(x => x.DifficultyLevelId)
+                .NotEmpty();
+
+            RuleFor(x => x.Title)
+                .NotEmpty()
+                .MaximumLength(200);
+
+            RuleFor(x => x.Slug)
+                .NotEmpty()
+                .MaximumLength(250);
+
+            RuleFor(x => x.Description)
+                .NotEmpty();
+
+            RuleFor(x => x.Constraints)
+                .NotEmpty();
+
+            RuleFor(x => x.TimeLimitInMilliseconds)
+                .GreaterThan(0);
+
+            RuleFor(x => x.MemoryLimitInMegabytes)
+                .GreaterThan(0);
+        }
+    }
+}
