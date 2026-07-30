a0fe58eee9091260c411282f53014965c653eaf3
Himanshu Maurya <himanshumaurya7781@gmail.com>
2026-07-30 17:19:36 +0530

    Add UnpublishProblem command and handler for managing problem unpublishing

---
+++ AlgoArena/AlgoArena.Application/Features/Problems/Commands/UnpublishProblem/UnpublishProblemCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace AlgoArena.Application.Features.Problems.Commands.UnpublishProblem
+{
+    /// <summary>
+    /// Command used to unpublish a programming problem.
+    /// </summary>
+    public sealed record UnpublishProblemCommand(Guid Id) : IRequest;
+}
