5d5f11f282aac341e1a3fd9c521439efafc3be74
Himanshu Maurya <himanshumaurya7781@gmail.com>
2026-07-30 17:26:00 +0530

    Add DependencyInjection class for registering persistence services

---
+++ AlgoArena/AlgoArena.Persistence/DependencyInjection/DependencyInjection.cs
@@ -0,0 +1,36 @@
+﻿using AlgoArena.Persistence.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AlgoArena.Persistence.DependencyInjection
+{
+    /// <summary>
+    /// Registers all persistence services.
+    /// </summary>
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<AlgoArenaDbContext>(options =>
+            {
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            // Feature registrations
+            services.AddProblemsPersistence();
+
+            // Future features
+            // services.AddIdentityPersistence();
+            // services.AddProfilesPersistence();
+            // services.AddLookupsPersistence();
+            // services.AddAnalyticsPersistence();
+            // services.AddSubmissionsPersistence();
+
+            return services;
+        }
+    }
+}
